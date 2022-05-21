ce4df1882e8735738fcc0aeb5803cf76c9087e0b
Türker Cakci <tuerkercakici@web.de>
2022-05-21 20:09:22 +0200

    added Device exit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import webbrowser
 import requests
 
 address = "192.168.2.120"
-val = 0
+isConnected = False
 
 
 def callAlexa():
@@ -20,40 +20,35 @@ def controlServo():
 
     p = GPIO.PWM(servoPIN, 50)  # GPIO 17 als PWM mit 50Hz
     p.start(2.5)  # Initialisierung
-    try:
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(12.5)
-    except KeyboardInterrupt:
-        p.stop()
-        GPIO.cleanup()
+    p.ChangeDutyCycle(12.5)
+    p.stop()
+    GPIO.cleanup()
 
 
 def ping():
+    global isConnected
     res = subprocess.call(['ping', '-c', '1', address])
     if res == 0:
         print("ping to", address, "OK")
-        controlServo()
-        callAlexa()
-        #global val
-        #val = 1
+        if not isConnected:
+            controlServo()
+            callAlexa()
+            isConnected = True
     elif res == 2:
         print("no response from", address)
+        isConnected = True
     else:
         print("ping to", address, "failed!")
+        isConnected = True
 
 
 def exit_handler():
-    print('Device entered Network')
+    print('ending Script')
     # webbrowser.open('https://trigger.esp8266-server.de/api/?id=541&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
-    controlServo()
+    # controlServo()
 
 
-while val == 0:
+while not isConnected:
     print("---pinging---")
     ping()
 
