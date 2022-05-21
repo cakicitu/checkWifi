8187d5340849f57c928188c0ec33668883c1f3de
Türker Cakci <tuerkercakici@web.de>
2022-05-21 19:32:51 +0200

    added alexa trigger

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import atexit
 import RPi.GPIO as GPIO
 import time
 import webbrowser
+import requests
 
 address = "192.168.2.120"
 val = 0
+
+
+def callAlexa():
+    requests.get('https://trigger.esp8266-server.de/api/?id=541&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
 
 
 def controlServo():
@@ -16,23 +21,13 @@ def controlServo():
     p = GPIO.PWM(servoPIN, 50)  # GPIO 17 als PWM mit 50Hz
     p.start(2.5)  # Initialisierung
     try:
-        while True:
-            p.ChangeDutyCycle(5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(7.5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(10)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(12.5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(10)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(7.5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(2.5)
-            time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
     except KeyboardInterrupt:
         p.stop()
         GPIO.cleanup()
@@ -42,8 +37,10 @@ def ping():
     res = subprocess.call(['ping', '-c', '1', address])
     if res == 0:
         print("ping to", address, "OK")
-        global val
-        val = 1
+        controlServo()
+        callAlexa()
+        #global val
+        #val = 1
     elif res == 2:
         print("no response from", address)
     else:
@@ -52,7 +49,7 @@ def ping():
 
 def exit_handler():
     print('Device entered Network')
-    webbrowser.open('https://trigger.esp8266-server.de/api/?id=541&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
+    # webbrowser.open('https://trigger.esp8266-server.de/api/?id=541&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
     controlServo()
 
 
