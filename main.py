4fd1c1d079040908b216559dbad14cac3aeda000
Türker Cakci <tuerkercakici@web.de>
2022-05-22 16:15:07 +0200

    test nmap

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
 import subprocess
-import atexit
-#import RPi.GPIO as GPIO
 import time
-import webbrowser
 import requests
-import psutil
-import socket
 import nmap
 import socket
 
@@ -18,30 +12,6 @@ def callAlexaConnected():
     requests.get('https://trigger.esp8266-server.de/api/?id=541&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
 
 
-def remote_ips():
-    '''
-    Returns the list of IPs for current active connections
-    '''
-
-    remote_ips = []
-
-    for process in psutil.process_iter():
-        try:
-            connections = process.connections(kind='inet')
-        except psutil.AccessDenied or psutil.NoSuchProcess or OSError:
-            pass
-        else:
-            for connection in connections:
-                if connection.raddr and connection.raddr[0] not in remote_ips:
-                    remote_ips.append(connection.raddr[0])
-
-    print()
-    if address in remote_ips:
-        print("true")
-    else:
-        print("false")
-    #return remote_ips
-
 def checkNetwork():
     ip_addr = address
     scanner = nmap.PortScanner()
@@ -49,17 +19,9 @@ def checkNetwork():
     scanner.scan(host, '1', '-v')
     print("IP Status: ", scanner[host].state())
 
-def remote_ip_present(ip):
-    if ip in remote_ips():
-        print("true")
-    else:
-        print("false")
-
-
 
 def callAlexaDisconnected():
     requests.get('https://trigger.esp8266-server.de/api/?id=542&hash=a5770e7bc8b2e5e4adcae74a3335c73d')
-
 
 
 def ping():
