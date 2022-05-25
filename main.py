5675b6f0f6a3e39fbe1987f5a6f73a4571e2b444
Türker Cakci <tuerkercakici@web.de>
2022-05-25 23:50:56 +0200

    stepper

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 from picamera import PiCamera
 from time import sleep
 import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+import time
 
 address = "192.168.2.120"
 isConnected = False
@@ -15,6 +17,34 @@ SENSOR_PIN = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
+
+def controllStepper():
+    ################################
+    # RPi and Motor Pre-allocations
+    ################################
+    #
+    # define GPIO pins
+    direction = 22  # Direction (DIR) GPIO Pin
+    step = 23  # Step GPIO Pin
+    EN_pin = 24  # enable pin (LOW to enable)
+
+    # Declare a instance of class pass GPIO pins numbers and the motor type
+    mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
+    GPIO.setup(EN_pin, GPIO.OUT)  # set enable pin as output
+
+    ###########################
+    # Actual motor control
+    ###########################
+    #
+    GPIO.output(EN_pin, GPIO.LOW)  # pull enable to low to enable motor
+    mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
+                         "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                         200,  # number of steps
+                         .0005,  # step delay [sec]
+                         False,  # True = print verbose output
+                         .05)  # initial delay [sec]
+
+    GPIO.cleanup()  # clear GPIO allocations after run
 
 def mein_callback(channel):
     # Hier kann alternativ eine Anwendung/Befehl etc. gestartet werden.
